d4f27dad82946ef2b2e0cad4a8910d209fb808e9
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-12 10:15:08 +0500

    Refactor chirp routes and remove friends functionality

--- index.js
+++ index.js
@@ -1,44 +1,81 @@
-import express from 'express'
+import express from "express";
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-    {
-        id: 1,
-        name: "Newton",
-    },
-    {
-        id: 2, 
-        name: "Ibrar",
-    },
-    {
-        id:3,
-        name: "hammad",
+app.use(express.json());
 
-    }
+
+const chirps = [
+  {
+    id: 1,
+    user: "ibzy_dev",
+    text: "First chirp, let's gooo ",
+  },
+  {
+    id: 2,
+    user: "Ibrar Ahmad",
+    text: "This is the second Chirp, let's move further",
+  },
 ];
 
-app.get('/friends', (req, res)=>{
-    res.json(friends); // data is treated as JavaScript \ 
+app.get("/chirps", (req, res) => {
+  res.status(200).json(chirps);
+});
 
-})
-app.get('/friends/:id', (req, res)=>{
-    const friendId = Number(req.params.id); //params gives you that ID.
-    const friend = friends[friendId];
+app.get('/chirp/:id', (req, res)=>{
+    const chirpId = Number(req.params.id)
+
+    const chirpData = chirps.find((chirp)=> chirp.id === chirpId);
 
 
-    if(friend){ 
-        res.status(200).json(friend);
+    if(chirpData){
+        res.status(200).json(chirpData)
     }
     else{
-        // what if /friend/2222/ no match found, express do it itself. 
-        res.status(404).json({
-            error: "Not Found"
+        res.status(402).json("Not Found");
+    }
+})
+
+app.post('/chirp', (req, res)=>{
+    const {user, text} = req.body;
+
+    if(!user || !text){
+        return res.status(400).json({
+            error: "User and Text are required"
         })
-        
-    } 
+    }
+
+    const newChrip = {
+        id: chirps.length+1,
+        user,
+        text,
+    }
+
+    chirps.push(newChrip);
+    console.log(chirps);
+    res.status(201).json(newChrip);
+     
 })
-app.listen(PORT, (req, res)=>{
-    console.log(`Server listening at PORT ${PORT}`);
+app.delete('/chirp/:id', (req, res)=>{
+    const id = Number(req.params.id);
+
+    const index = chirps.findIndex((chirp)=> chirp.id===id);
+
+    if(index==-1){
+        return res.status(404).json({
+            error: "No index found"
+        })
+    }
+
+    const deletedIndex = chirps.splice(index, 1);
+    console.log(chirps);
+    res.status(200).json({
+        message: "Chirp deleted Successfully"
+
+    })
 })
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server listening at PORT ${PORT}`);
+});
