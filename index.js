eae1ac745a38390ab5c82339d0153e291ff63417
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-17 17:02:07 +0500

    Set up Handlebars view engine and create index.hbs template; remove unused public/index.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import friendsRouter from "./Routers/friends.router.js";
 import express from 'express'
 const app = express();
 import path from "path";
+import { title } from "process";
 const __dirname = path.resolve();
 
 const PORT = 3000;
@@ -21,6 +22,20 @@ app.use((req, res, next) => {
 app.use('/site', express.static(path.join(__dirname, 'public')))
 
 app.use('/friends', friendsRouter);
+
+// HBS setup. 
+
+app.set('view engine', 'hbs');
+app.set('views', path.join( __dirname, 'views'));
+  
+// Route for hbs 
+app.get('/', (req, res)=>{
+    res.render('index', {
+        title: "My Friends are very clever",
+        caption: "Let's go Skiing"
+
+    });
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`Server listening at PORT ${PORT}`);
