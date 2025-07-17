a99eda1e5c68fc66614c419a5182ac8e9284e3d3
Ibrar Ahmad <90978293+IbrarAhmad969@users.noreply.github.com>
2025-07-17 09:26:28 +0500

    Refactor routing for friends functionality into a separate router module

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-import express from "express";
-import { getAllFriends } from "./controllers/friends.controller.js";
-import { getSpecificFriend } from "./controllers/friends.controller.js";
-import { postFriend } from "./controllers/friends.controller.js";
-import { deleteFriend } from "./controllers/friends.controller.js";
-
+import friendsRouter from "./Routers/friends.router.js";
+import express from 'express'
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.json());
@@ -16,15 +11,11 @@ app.use((req, res, next) => {
     next();
     const endTime = Date.now();
     const totalTime = endTime - start;
-    console.log(`The Method is ${req.method} - the URL is ${req.url} and total time spend in MS  is ${totalTime}`)
+    console.log(`The Method is ${req.method} - the URL is ${req.baseUrl} and total time spend in MS  is ${totalTime}`)
 
 })
 
-app.get("/friends", getAllFriends);
-app.get('/friend/:id', getSpecificFriend)
-app.post('/friends', postFriend)
-app.delete('/friend/:id', deleteFriend)
-
+app.use('/friends', friendsRouter);
 
 app.listen(PORT, (req, res) => {
     console.log(`Server listening at PORT ${PORT}`);
